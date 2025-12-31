c2cdb8c43f9f364dba7233b395f31e4e5861b234
Ankit Nandoliya <ankitnandoliya32@gmail.com>
2025-12-31 12:37:38 +0530

    Add stack folder structure

---
+++ stack/idnex.py
@@ -0,0 +1,22 @@
+#20 valid parentheses
+
+string = "(}"
+hashmap = {')':'(','}':'{',']':'['}
+
+stack = []
+def check(strig):
+    for c in string:
+        if not c in hashmap:
+            stack.append(c)
+        
+        else:
+            if not stack:
+                return False
+            else:
+                popped = stack.pop()
+                if popped != hashmap[c]:
+                    return False
+    return not stack
+
+print(check(string))
+
